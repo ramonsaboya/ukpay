c7101e153ce03d29757c16a01a664e55963fc21b
Ramon Saboya <ramonsaboya@fb.com>
2024-06-11 18:33:22 +0100

    calculate income tax and national insurance

--- src/company/meta/meta-income-tax.ts
+++ src/company/meta/meta-income-tax.ts
@@ -1,11 +1,75 @@
-import { CompensationElementType } from "src/compensation/element/compensation-element";
+import {
+  CompensationElementType,
+  IVirtualElement,
+} from "src/compensation/element/compensation-element";
 import IncomeTax from "src/compensation/element/income-tax";
 import Payslip, { IPayslip } from "src/compensation/income/payslip/payslip";
+import { TaxYear } from "src/hmrc/tax-year-builder";
+import {
+  CalculatedMonthCompensationValuesByElementType,
+  CalculatedCompensationValuesByMonth,
+} from "src/state/uk-pay-state";
+import TaxMonth from "src/taxMonth";
 
-export default class MetaIncomeTax extends IncomeTax implements IPayslip {
-  dependencies = new Set<CompensationElementType>();
+export default class MetaIncomeTax
+  extends IncomeTax
+  implements IPayslip, IVirtualElement
+{
+  dependencies = new Set([CompensationElementType.TAXABLE_PAY]);
 
   fromPayslip(payslip: Payslip): number {
     return payslip.deductions.getOrDefault("TAX PAID", 0);
+  }
+
+  fromState(
+    currentMonthValues: CalculatedMonthCompensationValuesByElementType,
+    taxMonth: TaxMonth,
+    previousMonthsValues: CalculatedCompensationValuesByMonth,
+    taxYear: TaxYear
+  ): number {
+    const cumulativeTaxablePay =
+      this.calculateCumulativeElementUntilLastMonth(
+        previousMonthsValues,
+        taxMonth,
+        CompensationElementType.TAXABLE_PAY
+      ) + currentMonthValues.get(CompensationElementType.TAXABLE_PAY)!;
+    const cumulativeTaxPaidUntilLastMonth =
+      this.calculateCumulativeElementUntilLastMonth(
+        previousMonthsValues,
+        taxMonth,
+        CompensationElementType.INCOME_TAX
+      );
+
+    const cumulativeTaxPaid = taxYear
+      .get(taxMonth)!
+      .taxRates.reduce((acc, band) => {
+        const cumulativeLowerLimit = (band.annualLowerLimit / 12) * taxMonth;
+        const cumulativeUpperLimit = (band.annualUpperLimit / 12) * taxMonth;
+
+        const payInBand = Math.max(
+          0,
+          Math.min(cumulativeTaxablePay, cumulativeUpperLimit) -
+            cumulativeLowerLimit
+        );
+        const taxInBand = payInBand * band.rate;
+
+        return acc + taxInBand;
+      }, 0);
+
+    return Math.max(0, cumulativeTaxPaid - cumulativeTaxPaidUntilLastMonth);
+  }
+
+  private calculateCumulativeElementUntilLastMonth(
+    previousMonthsValues: CalculatedCompensationValuesByMonth,
+    taxMonth: TaxMonth,
+    elementType: CompensationElementType
+  ): number {
+    return previousMonthsValues.reduce((acc, monthValues, month) => {
+      if (month >= taxMonth) {
+        return acc;
+      }
+
+      return acc + (monthValues.get(elementType) ?? 0);
+    }, 0);
   }
 }
--- src/company/meta/meta-net-pay.ts
+++ src/company/meta/meta-net-pay.ts
@@ -1,14 +1,63 @@
-import { CompensationElementType } from "src/compensation/element/compensation-element";
+import {
+  CompensationElementType,
+  IVirtualElement,
+} from "src/compensation/element/compensation-element";
 import NetPay from "src/compensation/element/net-pay";
 import Payslip, { IPayslip } from "src/compensation/income/payslip/payslip";
+import { CalculatedMonthCompensationValuesByElementType } from "src/state/uk-pay-state";
 
-export default class MetaNetPay extends NetPay implements IPayslip {
+export default class MetaNetPay
+  extends NetPay
+  implements IPayslip, IVirtualElement
+{
   dependencies = new Set([
-    CompensationElementType.TAXABLE_PAY,
-    CompensationElementType.PENSION_TOTAL_AMOUNT,
+    CompensationElementType.SALARY,
+    CompensationElementType.BONUS,
+    CompensationElementType.TAXABLE_BENEFITS,
+    CompensationElementType.RSU_TAX_OFFSET,
+    CompensationElementType.RSU_OVERWITHHELD_REFUND,
+    CompensationElementType.PENSION_EMPLOYEE_AMOUNT,
+    CompensationElementType.INCOME_TAX,
+    CompensationElementType.NATIONAL_INSURANCE,
   ]);
 
   fromPayslip(payslip: Payslip): number {
     return payslip.earnings.all() - payslip.deductions.all();
+  }
+
+  fromState(
+    currentMonthValues: CalculatedMonthCompensationValuesByElementType
+  ): number {
+    const salary = currentMonthValues.get(CompensationElementType.SALARY)!;
+    const bonus = currentMonthValues.get(CompensationElementType.BONUS)!;
+    const taxableBenefits = currentMonthValues.get(
+      CompensationElementType.TAXABLE_BENEFITS
+    )!;
+    const rsuTaxOffset = currentMonthValues.get(
+      CompensationElementType.RSU_TAX_OFFSET
+    )!;
+    const rsuOverwithheldRefund = currentMonthValues.get(
+      CompensationElementType.RSU_OVERWITHHELD_REFUND
+    )!;
+    const pensionEmployeeAmount = currentMonthValues.get(
+      CompensationElementType.PENSION_EMPLOYEE_AMOUNT
+    )!;
+    const incomeTax = currentMonthValues.get(
+      CompensationElementType.INCOME_TAX
+    )!;
+    const nationalInsurance = currentMonthValues.get(
+      CompensationElementType.NATIONAL_INSURANCE
+    )!;
+
+    return (
+      salary +
+      bonus +
+      taxableBenefits +
+      rsuTaxOffset +
+      rsuOverwithheldRefund -
+      pensionEmployeeAmount -
+      incomeTax -
+      nationalInsurance
+    );
   }
 }
